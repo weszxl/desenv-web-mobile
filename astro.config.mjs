8ceaead6cf0c25e5095eaee4c9c4a7af930ddaa0
Wesley Stark <wesleystarksf@outlook.com>
2026-06-24 19:13:37 -0300

    ...10

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // Quando comprar o domínio, insira a URL oficial aqui
+  site: 'https://www.seudominio.com.br',
+});
