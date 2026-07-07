4d33b7fefa040ce9812bc8f57049d3dc5689e557
Wesley Stark <142461482+weszxl@users.noreply.github.com>
2026-07-07 17:11:13 -0300

    Remove Astro configuration file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,1 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({
-  // Quando comprar o domínio, insira a URL oficial aqui
-  site: 'https://www.seudominio.com.br',
-});
